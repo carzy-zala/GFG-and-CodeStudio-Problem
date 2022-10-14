0b0bff61987153ec74fe51096ff20a0dc58bd221
crazy-zala <mr.zala2003@gmail.com>
2022-10-14 22:11:39 +0530

    new added in CS

---
+++ CodeStudio/Print Like A Wave.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    //Write your code here
+    vector<int> ans;
+    
+   for(int i=0;i<mCols;i++){
+       if(i&1){
+           for(int j=nRows-1;j>=0;j--){
+               ans.push_back(arr[j][i]);
+           }
+       }
+       
+       else{
+           for(int k=0;k<nRows;k++){
+               ans.push_back(arr[k][i]);
+           }
+       }
+   }
+    
+    return ans;
+}
