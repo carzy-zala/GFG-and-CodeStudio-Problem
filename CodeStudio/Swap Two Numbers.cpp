8ff5468a0451b6e9b784edfba73c94082a91d8ab
crazy-zala <mr.zala2003@gmail.com>
2022-10-17 21:19:53 +0530

    added  in CodeStudio

---
+++ CodeStudio/Swap Two Numbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+#include <utility>
+
+pair < int, int > swap(pair < int, int > swapValues) {
+    // Write your code here.
+    
+   
+        int temp=swapValues.first;
+        swapValues.first=swapValues.second;
+        swapValues.second=temp;
+   
+    
+    return swapValues;
+}
